ee6d5cb245a739475c5f32f27c72f4428484cae2
“279192434” <279192434@qq.com>
2021-04-07 01:30:01 +0800

    vos数据库备份

---
+++ backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#中文-UTF8
+
+#获取当前时间
+dateTime=`date +%Y%m%d`
+
+# 停止vos备份数据库全部文件
+service vos3000d stop
+service mbx3000d stop
+service callserviced stop
+service mysql stop
+tar czvf /root/backup/vos3000-mysql$dateTime.tar /var/lib/mysql/vos3000
+
+# 删除该文件夹下超过五天的文件
+find ./ -mtime +5 -name "*.tar" -exec rm -rf {} \;
+
+# 启动vos系统
+service vos3000d start
+service mbx3000d start
+service callserviced start
+service mysql start
+
+# #给自动备份脚本加执行权限
+# chmod 777 backup.sh
+
+# #利用系统crontab实现每天自动运行
+# crontab -e
+
+# #每天凌晨1点30分执行脚本，把下面的脚本输入到crontab -e里面
+# 30 01 * * * /root/backup.sh
+
+cd /root/backup/
+git add .
+git commit -m "vos数据库备份"
+git branch -M main
+git remote add origin git@github.com:279192434/backup.git
+git push -u origin main
+
