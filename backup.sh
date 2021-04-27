9f80f8eb11af8ca97cd4e1405ff4e49f7b8d95e6
279192434 <81734543+279192434@users.noreply.github.com>
2021-04-27 17:09:35 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -39,7 +39,7 @@ service mysql start
 # cd /root/
 # yum install -y git
 # #克隆服务器目录到本地
-# https://github.com/279192434/backup.git
+# git clone https://github.com/279192434/backup.git
 # git clone git@github.com:279192434/backup.git
 
 cd /root/backup/
